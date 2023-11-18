b2ef9c2cc2a9867eaf8e17a1820b76c5f336717d
Vikash <59873405+Vikaschaubey@users.noreply.github.com>
2023-11-18 10:24:03 +0530

    Update 04_insert.py

--- 04_insert.py
+++ 04_insert.py
@@ -4,7 +4,7 @@ cur = mysql.cursor()
 
 try:
     query="INSERT INTO student(stu_name,stu_class,stu_email) values(%s,%s,%s)"
-    t=[("Vikash","12th","vikas@gmail.com")]
+    t=["Vikash","12th","vikas@gmail.com"]
     cur.execute(query,t)
     mysql.commit()
     print("Row inserted")
